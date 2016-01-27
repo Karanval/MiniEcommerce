336ef3abcd5ac79709fb63ebedf50af5f09e9d9a
Karolina Arancibia Valuskova <yotu.tuy@gmail.com>
2016-01-27 13:48:00 -0400

    Se reviso el query mysql, ya esta funcionando.

--- add-customer-result.php
+++ add-customer-result.php
@@ -5,51 +5,65 @@
     <title>Add customer result</title>
   </head>
   <body>
+    <header class="result-header">
+      <h1 class="title">Registry confirmation</h1>
+    </header>
     <?php
 
-      if(isset($_GET['email']))
-      {
+      if(isset($_GET['email'])){
         $email = $_GET['email'];
+      } else{
+        $email = null;
       }
-      if(isset($_GET['password']))
-      {
+      if(isset($_GET['password'])){
         $password = $_GET['password'];
+      } else {
+        $password = null;
       }
-      if(isset($_GET['firstname']))
-      {
+      if(isset($_GET['firstname'])){
         $name = $_GET['firstname'];
+      } else {
+        $name = null;
       }
-      if(isset($_GET['lastname']))
-      {
+      if(isset($_GET['lastname'])){
         $lastname = $_GET['lastname'];
+      } else{
+        $lastname = null;
       }
-      if(isset($_GET['phone']))
-      {
+      if(isset($_GET['phone'])){
         $phone = $_GET['phone'];
+      } else {
+        $phone = null;
       }
-      if(isset($_GET['address']))
-      {
+      if(isset($_GET['address'])){
         $address = $_GET['address'];
+      } else {
+        $address = null;
       }
-      if(isset($_GET['city']))
-      {
+      if(isset($_GET['city'])){
         $city = $_GET['city'];
+      } else {
+        $city = null;
       }
-      if(isset($_GET['state']))
-      {
+      if(isset($_GET['state'])){
         $state = $_GET['state'];
+      } else {
+        $state = null;
       }
-      if(isset($_GET['postal_code']))
-      {
+      if(isset($_GET['postal_code'])){
         $postal_code = $_GET['postal_code'];
+      } else {
+        $postal_code = null;
       }
-      if(isset($_GET['country']))
-      {
+      if(isset($_GET['country'])){
         $country = $_GET['country'];
+      } else {
+        $country = null;
       }
-      if(isset($_GET['credit_limit']))
-      {
+      if(isset($_GET['credit_limit'])){
         $credit_limit = $_GET['credit_limit'];
+      } else {
+        $credit_limit = null;
       }
 
       $servername = "localhost";
@@ -64,17 +78,22 @@
 
       $sql = "INSERT INTO cliente (NOMBRE, APELLIDO_s, TELEFONO,
       DIRECCION, CIUDAD, ESTADO, CODIGO_POSTAL, PAIS, LIMITE_CREDITO, email,
-      password) VALUES (".$name.", ".$lastname.", ".$phone.", ".$address.", ".
-      $city.", ". $state.", ".$postal_code.", ".$country.", ".$credit_limit.", "
-      .$email.", ".$password.")";
-			$result = $conn->query($sql);
+      password) VALUES ('".$name."', '".$lastname."', '".$phone."', '".$address."', '".
+      $city."', '". $state."', '".$postal_code."', '".$country."', '".$credit_limit."', '"
+      .$email."', '".$password."')";
+      if(!empty($name) && !empty($lastname) && !empty($address) && !empty($city)
+      && !empty($country)){
+			  $result = $conn->query($sql);
+      }
       if($result){
-        echo "<label> insercion correcta </label>";
+        echo "<p class="."result-message"."> Registry correct </p>";
       } else {
-        echo "<label> insercion incorrecta </label>";
+        echo "<p class="."result-message"."> Registry incorrect, please review
+        the mandatory fields </p>";
       }
 
       $conn->close();
     ?>
+    <button class="continue" action="index.php">Continue</button>
   </body>
 </html>

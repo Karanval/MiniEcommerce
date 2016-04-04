ee6390bbae49a7cbabf38db6454a1612c71001b0
Karynn <yotu.tuy@gmail.com>
2016-04-03 20:30:37 -0400

    se arreglo el login

--- login-result.php
+++ login-result.php
@@ -20,7 +20,13 @@
       } else {
         $password = null;
       }
-      include("php/data-base-conexion.php");
+      $servername = "localhost";
+      $username = "root";
+      $db = "miniecommerce";
+      $conn = new mysqli($servername, $username, "", $db);
+      if ($conn->connect_error) {
+         echo "Connection failed: " . $conn->connect_error;
+       }
       if(!empty($login) && !empty($password)){
         $sql = "SELECT * FROM USUARIO WHERE LOGIN='".$login."' AND PASSWD ='$password'";
         $result = $conn->query($sql);
